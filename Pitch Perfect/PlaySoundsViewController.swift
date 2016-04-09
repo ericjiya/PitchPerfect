3af3dba60c1d83f68bbb3f4e4fa00755d1e37c75
jiya <jiya.young@gmail.com>
2016-04-09 11:36:42 -0400

    commit some new files

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-
+    var audioPlayer:AVAudioPlayer!
+    var receivedAudio:RecordedAudio!
+    
+    var audioEngine: AVAudioEngine!
+    var audioFile:AVAudioFile!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        audioPlayer = try! AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
+        audioPlayer.enableRate=true
+        
+        audioEngine = AVAudioEngine()
+        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +42,60 @@ class PlaySoundsViewController: UIViewController {
     }
     */
 
+    @IBAction func playSlowAudio(sender: UIButton) {
+        playWithDifferentRate(0.5)
+    }
+   
+    @IBAction func playFastAudio(sender: UIButton) {
+        playWithDifferentRate(1.5)
+    }
+    
+    //play audio as different rates
+    func playWithDifferentRate(rate: Float){
+        stopAllAudio()
+        
+        audioPlayer.rate = rate
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+    
+    
+    @IBAction func playDarthvaderAudio(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
+    //play audio as different pitches
+    func playAudioWithVariablePitch(pitch: Float){
+        stopAllAudio()
+        audioEngine.reset()
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        try! audioEngine.start()
+        
+        audioPlayerNode.play()
+    }
+    
+    @IBAction func stopAudioPlay(sender: UIButton) {
+        stopAllAudio()
+    }
+    
+    //stop audio player and engine
+    func stopAllAudio(){
+        audioPlayer.stop()
+        audioEngine.stop()
+    }
 }
--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecordSoundsViewController.swift
 //  Pitch Perfect
 //
 //  Created by jiya on 10/24/15.
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
     @IBOutlet weak var recordingInProgress: UILabel!
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder!
+    var recordedAudio: RecordedAudio!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,31 +24,69 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(animated: Bool) {
-        recordButton.enabled=true
-        stopButton.hidden=true
+        buttonClickAction(true, stopBtnHidden: true, recLableText: "Tap to Record")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        
     }
 
 
     @IBAction func recordAudio(sender: UIButton) {
-        stopButton.hidden=false
-        recordingInProgress.hidden = false
-        
-        recordButton.enabled=false
-        
-        //TODO: Record the user's voice
         print("in recordAudio")
         
+        buttonClickAction(false, stopBtnHidden: false, recLableText: "recording...")
+        
+        //Record the user's voice
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        let recordingName = "my_audio.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    func buttonClickAction(recBtnEnalbe: Bool, stopBtnHidden: Bool, recLableText: String){
+        recordButton.enabled=recBtnEnalbe
+        recordingInProgress.text=recLableText
+        stopButton.hidden=stopBtnHidden
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        if(flag){
+        //step 1 - Save the recorded audio
+        recordedAudio = RecordedAudio(filePathUrl: recorder.url,title: recorder.url.lastPathComponent!)
+        
+        //step 2 - Move to the next scene aka perform segue
+        self.performSegueWithIdentifier("stopRecording", sender: recordedAudio)
+        }else{
+            print("recording was not successfully")
+            recordButton.enabled=true
+            stopButton.hidden=true
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier=="stopRecording"){
+            let playSoundsVC: PlaySoundsViewController = segue.destinationViewController as! PlaySoundsViewController
+            let data = sender as! RecordedAudio
+            playSoundsVC.receivedAudio = data
+        }
     }
     
     @IBAction func stopRecordAudio(sender: UIButton) {
-        recordingInProgress.hidden=true
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance();
+        try! audioSession.setActive(false)
     }
-    
 }
-
